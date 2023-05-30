0d80950670c12b13140d453f8bf4137e1574afad
hyexu1608 <114460759+hyexu1608@users.noreply.github.com>
2023-05-30 08:05:12 +0000

    link validate

--- public/ttpets.js
+++ public/ttpets.js
@@ -20,6 +20,13 @@ async function sendPostRequest(url,data) {
 function buttonAction() {
   let username = document.getElementById("input1").value;
   let inputUrl = document.getElementById("input2").value;
+
+  if (!validateTikTokLink(inputUrl))
+  {
+    alert("Not a valid Tiktok video link.");
+  }
+
+  
   let nickname = document.getElementById("input3").value;
   let  dataToSent = username + " " + inputUrl + " " + nickname;
 
@@ -34,4 +41,16 @@ function buttonAction() {
     .catch(function(error) {
       console.log("Error occurred:", error)
     });
+}
+
+
+function validateTikTokLink(input) {
+  // Regular expression pattern to match TikTok links
+  var tikTokRegex = /^(?:https?:\/\/)?(?:www\.)?tiktok\.com\/@([a-zA-Z0-9._-]+)\/video\/(\d+)\?/;
+
+  // Check if the input matches the TikTok link pattern
+  var match = input.match(tikTokRegex);
+
+  // Return true if the input is a valid TikTok link, false otherwise
+  return match !== null;
 }
